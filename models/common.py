d0e2e92ce6c3599f137041520717f0468b7446e1
Ruicong Zhu <e0324850@u.nus.edu>
2021-09-20 21:10:20 +0800

    basic changes

--- models/common.py
+++ models/common.py
@@ -97,6 +97,33 @@ def bn(num_features):
     return nn.BatchNorm2d(num_features)
 
 
+# def conv(in_f, out_f, kernel_size, stride=1, bias=True, pad='zero', downsample_mode='stride'):
+#     downsampler = None
+#     if stride != 1 and downsample_mode != 'stride':
+#
+#         if downsample_mode == 'avg':
+#             downsampler = nn.AvgPool2d(stride, stride)
+#         elif downsample_mode == 'max':
+#             downsampler = nn.MaxPool2d(stride, stride)
+#         elif downsample_mode  in ['lanczos2', 'lanczos3']:
+#             downsampler = Downsampler(n_planes=out_f, factor=stride, kernel_type=downsample_mode, phase=0.5, preserve_size=True)
+#         else:
+#             assert False
+#
+#         stride = 1
+#
+#     padder = None
+#     to_pad = int((kernel_size - 1) / 2)
+#     if pad == 'reflection':
+#         padder = nn.ReflectionPad2d(to_pad)
+#         to_pad = 0
+#
+#     convolver = nn.Conv2d(in_f, out_f, kernel_size, stride, padding=to_pad, bias=bias)
+#
+#     layers = filter(lambda x: x is not None, [padder, convolver, downsampler])
+#     return nn.Sequential(*layers)
+
+
 def conv(in_f, out_f, kernel_size, stride=1, bias=True, pad='zero', downsample_mode='stride'):
     downsampler = None
     if stride != 1 and downsample_mode != 'stride':
@@ -105,8 +132,9 @@ def conv(in_f, out_f, kernel_size, stride=1, bias=True, pad='zero', downsample_m
             downsampler = nn.AvgPool2d(stride, stride)
         elif downsample_mode == 'max':
             downsampler = nn.MaxPool2d(stride, stride)
-        elif downsample_mode  in ['lanczos2', 'lanczos3']:
-            downsampler = Downsampler(n_planes=out_f, factor=stride, kernel_type=downsample_mode, phase=0.5, preserve_size=True)
+        elif downsample_mode in ['lanczos2', 'lanczos3']:
+            downsampler = Downsampler(n_planes=out_f, factor=stride, kernel_type=downsample_mode, phase=0.5,
+                                      preserve_size=True)
         else:
             assert False
 
@@ -117,10 +145,8 @@ def conv(in_f, out_f, kernel_size, stride=1, bias=True, pad='zero', downsample_m
     if pad == 'reflection':
         padder = nn.ReflectionPad2d(to_pad)
         to_pad = 0
-  
-    # convolver = nn.Conv2d(in_f, out_f, kernel_size, stride, padding=to_pad, bias=bias)
-    convolver = BayesianConv2d(in_f, out_f, (kernel_size, kernel_size), stride=stride, padding=to_pad, bias=bias)
 
+    convolver = BayesianConv2d(in_f, out_f, (kernel_size, kernel_size), stride=stride, padding=to_pad, bias=bias)
 
     layers = filter(lambda x: x is not None, [padder, convolver, downsampler])
     return nn.Sequential(*layers)
