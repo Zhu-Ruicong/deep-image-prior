25d14697b3d0dbe157e4160ae5e348733a33e5e9
ruicong <traviszhuruicong@gmail.com>
2021-12-26 14:00:42 +0800

    dirichlet with kl

--- models/dirichlet.py
+++ models/dirichlet.py
@@ -6,6 +6,7 @@ from torch.distributions.kl import kl_divergence
 import numpy as np
 
 CHANNEL_EXP_SUMS = [1,1,1]
+CURRENT_SIZE = 128
 
 
 class ExponentOutputLayer(nn.Module):
@@ -36,11 +37,11 @@ class DirichletLayer(nn.Module):
 #     global CHANNEL_EXP_SUMS
 #     CHANNEL_EXP_SUMS = out
 
-def get_softmax_gt(img_np, img_torch):
-    t = torch.reshape(img_torch, [3, 128 * 128])
+def get_softmax_gt(img_torch, img_np):
+    t = torch.reshape(img_torch, [3, CURRENT_SIZE * CURRENT_SIZE])
     softmax_torch = torch.softmax(t, dim=1)
     softmax_torch = torch.reshape(softmax_torch, img_torch.size())
-    tt = img_np.reshape([3, 128 * 128])
+    tt = img_np.reshape([3, CURRENT_SIZE * CURRENT_SIZE])
     exp_sums = []
     for i in range(3):
         ttt = tt[i]
@@ -50,15 +51,15 @@ def get_softmax_gt(img_np, img_torch):
 
 
 def get_scaled_gt(img_torch):
-    t = torch.reshape(img_torch, [3, 128 * 128])
+    t = torch.reshape(img_torch, [3, CURRENT_SIZE * CURRENT_SIZE])
     sums = t.sum(1)
     scaled = torch.matmul(torch.diag(1 / sums), t)
     scaled = torch.reshape(scaled, img_torch.size())
     return scaled, sums
 
 
-def recover(img_np, sums):
-    t = img_np.reshape([3, 128 * 128])
+def recover_scale(img_np, sums):
+    t = img_np.reshape([3, CURRENT_SIZE * CURRENT_SIZE])
     output = []
     for i in range(3):
 
@@ -67,12 +68,24 @@ def recover(img_np, sums):
         prev_sum = sums[i]
         channel = np.array([xi * prev_sum / cur_sum for xi in tt])
         output.append(channel)
-    output = np.array(output).reshape([3, 128, 128])
+    output = np.array(output).reshape([3, CURRENT_SIZE, CURRENT_SIZE])
+    return output
+
+
+def recover_softmax(img_np, exp_sums, scalar):
+    t = img_np.reshape([3, 128 * 128])
+    output = []
+    for i in range(3):
+        tt = t[i]
+        exp_sum = exp_sums[i]
+        channel = np.array([math.log(xi * exp_sum / scalar) for xi in tt])
+        output.append(channel)
+    output = np.array(output).reshape([3, CURRENT_SIZE, CURRENT_SIZE])
     return output
 
 
 def dirichlet_kl_divergence(p, q, scalar):
-    q_reshaped = torch.reshape(q, [3, 128 * 128]) * scalar
-    p_reshaped = torch.reshape(p, [3, 128 * 128])
+    q_reshaped = torch.reshape(q, [3, CURRENT_SIZE * CURRENT_SIZE]) * scalar
+    p_reshaped = torch.reshape(p, [3, CURRENT_SIZE * CURRENT_SIZE])
     temp = kl_divergence(Dirichlet(p_reshaped), Dirichlet(q_reshaped)).sum()
     return temp.sum()
